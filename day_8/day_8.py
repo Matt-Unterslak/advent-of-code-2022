f36fadfc47bd6fa3168a9d00d224196d04caf303
matthewunterslak <matthew@melio.ai>
2022-12-08 17:32:02 +0200

    refactor: improved time complexity for day_8

--- day_8/day_8.py
+++ day_8/day_8.py
@@ -1,3 +1,4 @@
+import cProfile
 from typing import List, Dict
 import numpy as np
 from functools import reduce
@@ -18,78 +19,102 @@ def list_read_buffer(file: str):
     return data
 
 
+def get_visibility_and_scenic_scores(numpy_grid, need_scenic=False):
+    visible = 2 * numpy_grid.shape[0] + 2 * (numpy_grid.shape[0] - 2)
+    # print(f"{visible=}")
+    scenic_trees = []
+    highest_scenic_score = 0
+    for row in range(1, numpy_grid.shape[0] - 1):
+        for col in range(1, numpy_grid.shape[1] - 1):
+            grid_pos = [row, col]
+            tree = numpy_grid[row, col]
+
+            up_visible = down_visible = left_visible = right_visible = True
+            scenic_scores = [0, 0, 0, 0]
+            for i in range(1, numpy_grid.shape[0] - 1):
+                # UP
+                if up_visible is True:
+                    up = (
+                        numpy_grid[row - i, col] if row - i >= 0 else numpy_grid[0, col]
+                    )
+                    if tree <= up:
+                        up_visible = False
+                    if row - i >= 0:
+                        scenic_scores[0] += 1
+
+                # DOWN
+                if down_visible is True:
+                    down = (
+                        numpy_grid[row + i, col]
+                        if row + i <= numpy_grid.shape[0] - 1
+                        else numpy_grid[numpy_grid.shape[0] - 1, col]
+                    )
+                    if tree <= down:
+                        down_visible = False
+                    if row + i <= numpy_grid.shape[0] - 1:
+                        scenic_scores[1] += 1
+
+                # LEFT
+                if left_visible is True:
+                    left = (
+                        numpy_grid[row, col - i] if col - i >= 0 else numpy_grid[row, 0]
+                    )
+                    if tree <= left:
+                        left_visible = False
+                    if col - i >= 0:
+                        scenic_scores[2] += 1
+
+                # RIGHT
+                if right_visible is True:
+                    right = (
+                        numpy_grid[row, col + i]
+                        if col + i <= numpy_grid.shape[1] - 1
+                        else numpy_grid[row, numpy_grid.shape[1] - 1]
+                    )
+                    if tree <= right:
+                        right_visible = False
+                    if col + i <= numpy_grid.shape[1] - 1:
+                        scenic_scores[3] += 1
+                if (
+                    not up_visible
+                    and not down_visible
+                    and not left_visible
+                    and not right_visible
+                ):
+                    break
+
+            if up_visible or down_visible or left_visible or right_visible:
+                total_scenic_score = (
+                    scenic_scores[0]
+                    * scenic_scores[1]
+                    * scenic_scores[2]
+                    * scenic_scores[3]
+                )
+                if total_scenic_score > highest_scenic_score:
+                    highest_scenic_score = total_scenic_score
+                if need_scenic:
+                    scenic_trees.append([row, col, tree, total_scenic_score])
+                visible += 1
+            # print(row, col, tree, up_visible, down_visible, left_visible, right_visible)
+    return visible, highest_scenic_score, scenic_trees
+
+
 txt_file: str = "data/data_8.txt"
 # txt_file: str = "./data/test.txt"
 file_data: List[int] = list_read_buffer(txt_file)
 # print(f"{file_data=}")
 
-numpy_grid = np.array(file_data)
-print(f"{numpy_grid.shape=}")
+grid = np.array(file_data)
+print(f"{grid.shape=}")
 
-# print(numpy_grid)
+# print(grid)
 
 print("Part 1")
 print("--------------------------------------")
 
-visible = 2 * numpy_grid.shape[0] + 2 * (numpy_grid.shape[0] - 2)
-# print(f"{visible=}")
-scenic_trees = []
-highest_scenic_score = 0
-for row in range(1, numpy_grid.shape[0] - 1):
-    for col in range(1, numpy_grid.shape[1] - 1):
-        grid_pos = [row, col]
-        tree = numpy_grid[row, col]
-
-        up_visible = down_visible = left_visible = right_visible = True
-        scenic_scores = [0, 0, 0, 0]
-        for i in range(1, numpy_grid.shape[0] - 1):
-            # UP
-            if up_visible is True:
-                up = numpy_grid[row - i, col] if row - i >= 0 else numpy_grid[0, col]
-                if tree <= up:
-                    up_visible = False
-                if row - i >= 0:
-                    scenic_scores[0] += 1
-
-            # DOWN
-            if down_visible is True:
-                down = (
-                    numpy_grid[row + i, col]
-                    if row + i <= numpy_grid.shape[0] - 1
-                    else numpy_grid[numpy_grid.shape[0] - 1, col]
-                )
-                if tree <= down:
-                    down_visible = False
-                if row + i <= numpy_grid.shape[0] - 1:
-                    scenic_scores[1] += 1
-
-            # LEFT
-            if left_visible is True:
-                left = numpy_grid[row, col - i] if col - i >= 0 else numpy_grid[row, 0]
-                if tree <= left:
-                    left_visible = False
-                if col - i >= 0:
-                    scenic_scores[2] += 1
-
-            # RIGHT
-            if right_visible is True:
-                right = (
-                    numpy_grid[row, col + i]
-                    if col + i <= numpy_grid.shape[1] - 1
-                    else numpy_grid[row, numpy_grid.shape[1] - 1]
-                )
-                if tree <= right:
-                    right_visible = False
-                if col + i <= numpy_grid.shape[1] - 1:
-                    scenic_scores[3] += 1
-
-        if up_visible or down_visible or left_visible or right_visible:
-            total_scenic_score = reduce(lambda x, y: x * y, scenic_scores)
-            if total_scenic_score > highest_scenic_score:
-                highest_scenic_score = total_scenic_score
-            scenic_trees.append([row, col, tree, total_scenic_score])
-            visible += 1
-        # print(row, col, tree, up_visible, down_visible, left_visible, right_visible)
+visible, highest_scenic_score, scenic_trees = get_visibility_and_scenic_scores(
+    numpy_grid=grid
+)
 
 print()
 print(f"{visible=}")
@@ -100,3 +125,5 @@ print("Part 2")
 print("--------------------------------------")
 # print(f"{scenic_trees=}")
 print(f"{highest_scenic_score=}")
+
+cProfile.run("get_visibility_and_scenic_scores(numpy_grid=grid)")
