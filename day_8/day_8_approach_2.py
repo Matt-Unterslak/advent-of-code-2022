513836f8372dc2d28b926ccb92d61227f928b68c
matthewunterslak <matthew@melio.ai>
2022-12-09 22:02:43 +0200

    chore: tried alternative approach to day 8 and proved numpy is faster

--- day_8/day_8_approach_2.py
+++ day_8/day_8_approach_2.py
@@ -1,7 +1,6 @@
 import cProfile
-from typing import List, Dict
+from typing import List
 import numpy as np
-from functools import reduce
 
 
 def _count_generator(reader):
@@ -21,7 +20,6 @@ def list_read_buffer(file: str):
 
 def get_visibility_and_scenic_scores(numpy_grid, need_scenic=False):
     visible = 2 * numpy_grid.shape[0] + 2 * (numpy_grid.shape[0] - 2)
-    # print(f"{visible=}")
     scenic_trees = []
     highest_scenic_score = 0
     for row in range(1, numpy_grid.shape[0] - 1):
@@ -95,17 +93,70 @@ def get_visibility_and_scenic_scores(numpy_grid, need_scenic=False):
                 if need_scenic:
                     scenic_trees.append([row, col, tree, total_scenic_score])
                 visible += 1
-            # print(row, col, tree, up_visible, down_visible, left_visible, right_visible)
     return visible, highest_scenic_score, scenic_trees
+
+
+def visibility_scenic_score_approach_2(file_data):
+    row_length = len(file_data[0])
+    edge = 2 * row_length + 2 * (row_length - 2)
+    best_scenic_score = 0
+    for ind, row in enumerate(file_data):
+        if ind == 0 or ind == row_length - 1:
+            continue
+        for tree_ind, tree in enumerate(row):
+            if tree_ind == 0 or tree_ind == row_length - 1:
+                continue
+            left = file_data[ind][0:tree_ind] if tree_ind > 1 else [file_data[ind][0]]
+            right = file_data[ind][tree_ind + 1 :]
+            up = (
+                [x[tree_ind] for x in file_data[0:ind]]
+                if ind > 1
+                else [file_data[0][tree_ind]]
+            )
+            down = [x[tree_ind] for x in file_data[ind + 1 :]]
+            up_visible = [tree > x for x in up]
+            down_visible = [tree > x for x in down]
+            left_visible = [tree > x for x in left]
+            right_visible = [tree > x for x in right]
+
+            left_ind = next((i + 1 for i, e in enumerate(left_visible) if not e), -1)
+            right_ind = next((i + 1 for i, e in enumerate(right_visible) if not e), -1)
+            up_ind = next((i + 1 for i, e in enumerate(up_visible) if not e), -1)
+            down_ind = next((i + 1 for i, e in enumerate(down_visible) if not e), -1)
+
+            visibility_scores = [left_ind, right_ind, up_ind, down_ind]
+            if -1 in visibility_scores:
+                left_ind_score = next(
+                    (i + 1 for i, e in enumerate(left_visible) if not e),
+                    len(left_visible),
+                )
+                right_ind_score = next(
+                    (i + 1 for i, e in enumerate(right_visible) if not e),
+                    len(right_visible),
+                )
+                up_ind_score = next(
+                    (i + 1 for i, e in enumerate(up_visible) if not e), len(up_visible)
+                )
+                down_ind_score = next(
+                    (i + 1 for i, e in enumerate(down_visible) if not e),
+                    len(down_visible),
+                )
+                scenic_score = (
+                    left_ind_score + right_ind_score + up_ind_score + down_ind_score
+                )
+                if scenic_score > best_scenic_score:
+                    best_scenic_score = scenic_score
+                edge += 1
+    return edge, best_scenic_score
 
 
 txt_file: str = "data/data_8.txt"
 # txt_file: str = "./data/test.txt"
-file_data: List[int] = list_read_buffer(txt_file)
+raw_data: List[List[int]] = list_read_buffer(txt_file)
 # print(f"{file_data=}")
 
-grid = np.array(file_data)
-print(f"{grid.shape=}")
+grid = np.array(raw_data)
+# print(f"{grid.shape=}")
 
 # print(grid)
 
@@ -116,6 +167,11 @@ visible, scenic_highest_score, scenic_trees = get_visibility_and_scenic_scores(
     numpy_grid=grid
 )
 
+visible_trees, scenic_tree = visibility_scenic_score_approach_2(raw_data)
+
+
+print(f"{visible_trees=}")
+
 print()
 print(f"{visible=}")
 
@@ -125,5 +181,8 @@ print("Part 2")
 print("--------------------------------------")
 # print(f"{scenic_trees=}")
 print(f"{scenic_highest_score=}")
+print(f"{scenic_tree=}")
+
 
 cProfile.run("get_visibility_and_scenic_scores(numpy_grid=grid)")
+cProfile.run("visibility_scenic_score_approach_2(raw_data)")
